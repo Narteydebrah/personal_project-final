7b4e7ae8fac8e155f981a1dd9af60357906cd33c
nartey_debrah <79046232+Narteydebrah@users.noreply.github.com>
2021-12-11 10:47:35 +1200

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -5,7 +5,7 @@
         // username => root
         // password => empty
         // database name => staff
-        $conn1 = mysqli_connect("localhost", "root", "", "personal_project");
+        $conn1 = mysqli_connect("localhost", "root", "Nuclearbomb2.", "personal_project");
 
         // Check connection
         if ($conn1 === false) {
